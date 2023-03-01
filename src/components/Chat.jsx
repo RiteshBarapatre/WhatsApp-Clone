e4389bdd562869a2e5abff100cf367129b119d09
RiteshBarapatre <riteshbarapatre543@gmail.com>
2023-03-01 15:47:06 +0530

    frontEnd Complete

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import { AttachFile, InsertEmoticon, Mic, MoreVert, Search, Send, SmartButtonRounded } from '@mui/icons-material'
+import { Avatar, Button, IconButton } from '@mui/material'
+import React, { useState } from 'react'
 import '../css/chat.css'
 
 const Chat = () => {
+
+  const [input,setInput] = useState('')
+  
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className="chat_header">
+        <Avatar/>
+        <div className="chat_headerInfo">
+          <h3>Room Name</h3>
+          <p>last seen</p>
+        </div>
+        <div className="chat_headerRight">
+          <IconButton>
+            <AttachFile/>
+          </IconButton>
+          <IconButton>
+            <MoreVert/>
+          </IconButton>
+          <IconButton>
+            <Search/>
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat_body">
+        <p className='chat_message'>
+        <span className='chat_name'>Ritesh</span>
+        This is a message
+        <span className='chat_timestamp'>
+          {new Date().toLocaleTimeString ()}
+        </span>
+        </p>
+        <p className='chat_message chat_receiver'>
+        <span className='chat_name'>Ritesh</span>
+        This is a message
+        <span className='chat_timestamp'>
+          {new Date().toLocaleTimeString ()}
+        </span>
+        </p>
+        </div>
+        <div className="chat_footer">
+          <InsertEmoticon/>
+          <form>
+            <input type="text" name="" id="" placeholder='Type a message'
+              onChange={(e)=> setInput(e.target.value)}
+              value = {input}
+            />
+            <button type='submit'></button>
+          </form>
+            <Mic/>
+      </div>
+    </div>
   )
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,6 +4,8 @@ import ChatIcon from '@mui/icons-material/Chat';
 import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { IconButton, Avatar } from '@mui/material';
+import { Search  } from '@mui/icons-material';
+import SidebarChat from './SidebarChat';
 
 const Sidebar = () => {
   return (
@@ -22,7 +24,18 @@ const Sidebar = () => {
         </IconButton>
         </div>
     </div>
+    <div className="sidebar_search">
+      <div className="sidebar_searchContainer">
+        <Search/>
+        <input type="text" name="" id="" placeholder='Search or start new chat'/>
+      </div>
+    </div>
+    <div className="sidebar_chats">
 
+    <SidebarChat/>
+    <SidebarChat/>
+    <SidebarChat/>
+    </div>
     </div>
   )
 }
